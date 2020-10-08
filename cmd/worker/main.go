13095648dd4400aefec2a63b2380bd0a7ac2e357
Ngô Trường Sinh <sinhnt@bap.jp>
2020-10-08 09:52:22 +0700

    add file logging

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"github.com/tk1122/cloud-gaming/pkg/worker"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("http://localhost:8000")
+	f, err := os.OpenFile("all.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = f.Close()
+	}()
+
+	mw := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(mw)
+
+	log.Println("http://localhost:8000")
 	log.Fatal(http.ListenAndServe(":8000", worker.Router))
 	//fmt.Println("https://localhost:8000")
 	//log.Fatal(http.ListenAndServeTLS(":8000", "cert.pem", "key.pem", worker.Router))
